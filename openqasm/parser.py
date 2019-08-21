6f22c7eeab3667ee15054c3f61f8454a8c9ee151
Eddie Schoute <eddieschoute@gmail.com>
2019-08-21 18:35:17 -0400

    Addded basic files for parsing a grammar

---
+++ openqasm/parser.py
@@ -0,0 +1,21 @@
+from lark import Lark
+from pkg_resources import resource_string
+
+"""The grammar is imported from the openqasm.lark file as a string."""
+GRAMMAR = resource_string(__name__, 'openqasm.lark').decode()
+
+text = """OPENQASM 2.0;
+include "qelib1.inc";
+gate pre q { } // pre-rotation
+gate post q { } // post-rotation qreg q[1];
+creg c[1];
+pre q[0];
+barrier q;
+h q[0];
+barrier q;
+post q[0];
+measure q[0] -> c[0];
+"""
+
+calc_parser = Lark(GRAMMAR)
+calc = calc_parser.parse
