f72e50369555725fce6e23b98bfabd6c3de92137
Eddie Schoute <eddieschoute@gmail.com>
2019-08-22 16:14:26 -0400

    Refining the grammar
    Simplify the parse tree to a nice shape

--- openqasm/parser.py
+++ openqasm/parser.py
@@ -17,5 +17,30 @@ post q[0];
 measure q[0] -> c[0];
 """
 
-openqasm_parser = Lark(GRAMMAR)
+openqasm_parser = Lark(GRAMMAR, maybe_placeholders=True)
 openqasm = openqasm_parser.parse
+
+text = """
+OPENQASM 2.0;
+qreg q[3];
+creg c0[1];
+creg c1[1];
+creg c2[1];
+// optional post-rotation for state tomography gate post q { }
+u3(0.3,0.2,0.1) q[0];
+h q[1];
+cx q[1],q[2];
+barrier q;
+cx q[0],q[1];
+h q[0];
+measure q[0] -> c0[0];
+measure q[1] -> c1[0];
+if(c0==1) z q[2];
+if(c1==1) x q[2];
+post q[2];
+measure q[2] -> c2[0];
+U(pi*sin(3*exp(1)^pi)) q;
+"""
+res = openqasm(text)
+print(res)
+print(res.pretty())
